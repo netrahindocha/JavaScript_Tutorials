5e92249ff4f94b8ab47a4763a00b4cc0332a0946
netrahindocha <90775543+netrahindocha@users.noreply.github.com>
2022-07-30 23:59:29 +0530

    Add files via upload

---
+++ Manipulating Websites Using JS Window Object/index.js
@@ -0,0 +1,66 @@
+console.log('This is tut 11');
+
+//Alert
+let a = window;
+// alert("Hello Netra");
+// console.log(a);
+
+// Prompt 
+// a = prompt('This will destroy your computer. Type your name');
+// console.log('Your name is ' + a);
+
+// Confirm 
+// a = confirm('Are you sure you want to delete this page?');
+// console.log(a);
+
+// Inside Window Document
+a = window.document;
+console.log(a);
+
+// Window innerHeight
+a = window.innerHeight; // or a = innerHeight;
+console.log(a);
+
+// Window innerWidth 
+a = window.innerWidth; // or a = innerWidth;
+console.log(a);
+
+// ScrollX & ScrollY (only works when scrollbar is present)
+
+a = scrollX;
+console.log(a);
+
+// Location
+
+a = location;
+console.log(a);
+
+// location.reload() in console reloads the page 
+
+// But using it inside the script makes the page to reload continuously 
+// a = location.reload();
+// console.log(a);
+
+// location.href in console returns the url of the page and does the same for script
+a = location.href;
+console.log(a);
+
+// location.toString() does the same thing 
+a = location.toString();
+console.log(a);
+
+// location.href in console helps to reach any website 
+// location.href = "//google.com"; (in console only)
+
+a = window.history;
+console.log(a);
+
+// history.go(-1) in console helps to go back to the previous page for example google.com page in our case
+
+// history.go(1) in console helps to go forward to the next page for index.html page in our case
+
+// history.go works like back and forward arrow buttons in chrome 
+
+
+
+
