ba94e76c8dcce773d08c9559b4993fd27dab2a57
netrahindocha <90775543+netrahindocha@users.noreply.github.com>
2022-08-14 13:26:00 +0530

    Add files via upload

---
+++ Callback Functions/index.js
@@ -0,0 +1,33 @@
+console.log("This is tut 37");
+
+// Pretend that this response is coming from the server
+const students = [
+    {name: "Harry", subject: "JavaScript"},
+    {name: "Rohan", subject: "Machine Learning"}
+]
+
+
+function enrollStudent(student, callback){
+    setTimeout(function(){
+        students.push(student);
+        console.log("Student has been enrolled");
+        callback();
+    }, 1000);
+}
+
+function getStudents(){
+    setTimeout(function(){
+        let str = "";
+        students.forEach(function(student){
+            str += `<li> ${student.name} </li>`
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log("Students have been fetched");
+    }, 5000);
+}
+
+let newStudent = {name: "Sunny", subject: "Python"};
+enrollStudent(newStudent, getStudents);
+// getStudents();
+
+// Callback function helps to call the second function just after the first function is called 
